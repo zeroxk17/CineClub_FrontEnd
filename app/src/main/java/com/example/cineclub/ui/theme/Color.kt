95fb9af2c51c51b0dfb4e884b9c30eff228b3fba
sebastianhernandez206 <sebastianhernandezoso206@gmail.com>
2026-06-08 15:19:26 -0600

    Correccion de Color.kt

--- app/src/main/java/com/example/cineclub/ui/theme/Color.kt
+++ app/src/main/java/com/example/cineclub/ui/theme/Color.kt
@@ -2,9 +2,9 @@ package com.example.cineclub.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val DarkBackground = Color(0密0E1116)
-val DarkSurface = Color(0密1B1F24)
-val TealAccent = Color(0密2DB3A6)
+val DarkBackground = Color(0xFF0E1116)
+val DarkSurface = Color(0xFF1B1F24)
+val TealAccent = Color(0xFF2DB3A6)
 
 // Colores requeridos por Material3 complementarios
 val DarkPrimary = TealAccent
